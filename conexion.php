f7141a35e011a91fd02519c5f0b6c4a56455ac04
Alexis Goatache <ag297@hotmail.com>
2017-05-06 00:47:01 -0400

    Fusion de los archivos de conexion

--- conexion.php
+++ conexion.php
@@ -1,5 +1,14 @@
  <?php
 //1. CONECTAR MYSQL CON EL MANEJADOR DE BD
 $conectar=mysqli_connect('localhost','root','','bdempresas');
+
+function conectar(){
+  //1. CONECTAR MYSQL CON EL MANEJADOR DE BD
+  mysqli_connect ('localhost','root','');
+  //2. SELECCIONAR LA BD
+  mysqli_select_db('bdempresas');}
+
+function desconectar(){
+    mysql_close();}
 ?>
 
